90075923cc045b8d3c4866339a2d6abe7a3e39f3
Breno de Oliveira Bargamasco <214171584+bargamasco@users.noreply.github.com>
2026-03-20 16:32:08 -0300

    Update label for Sankey V2 visualization

---
+++ sankey_v2.js
@@ -0,0 +1,10 @@
+looker.visualizations.add({
+  id: "sankey_v2",
+  label: "Sankey V2",
+  options: {
+    color_range: {
+      type: "array",
+      label: "Color Range",
+      display: "colors",
+      default: ["#3EB0D5", "#B1399E", "#C2DD67", "#592EC2", "#4276BE"]
+    }
